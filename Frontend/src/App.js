0be7b134515f2ccab30cb597ac2169cb9da22794
VIJAYALAKSHMI R <146066129+Vijijeeva@users.noreply.github.com>
2025-10-15 13:09:39 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import Settings from "./components/Settings";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
